7fd49158b4f9143dedf5892ebf68f814495def09
BeeeeFx <139230284+BeeeeFx@users.noreply.github.com>
2024-01-10 18:35:13 +0100

    1.0
    Changing Original Launcher

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+        const distroURL = 'https://beefx-hosts.websr.fr/nebula/distribution.json/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
